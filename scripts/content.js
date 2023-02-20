a2677e89f3cf0c73518fa79432d60c49ab5610b1
hengmhs <hengmhs@gmail.com>
2023-02-20 21:09:15 +0800

    Add left right keys

--- scripts/content.js
+++ scripts/content.js
@@ -6,19 +6,19 @@ const showDetails = document.querySelectorAll("div.showDetails")
 const allLinks = document.querySelectorAll("div.allLinks")
 let currentPage = document.querySelector(".currentPage").innerText
 
-/*
-
 console.log("The Current Page: " + currentPage)
 
 let checkKey = function(e) {
   if (e.keyCode == '37' && currentPage > 1) {
      // left arrow
+     console.log('Left Works')
      goToPage(currentPage-1)
      // Requires loading the js script first
      // https://stackoverflow.com/questions/44760732/chrome-extension-run-extensions-script-after-all-other-page-scripts-have-loade
   }
-  else if (e.keyCode == '39') {
+  if (e.keyCode == '39') {
      // right arrow
+     console.log('Right Works')
      goToPage(currentPage+1)
   }
 }
@@ -28,7 +28,6 @@ let main = function (){
 }
 
 window.onload = main
-*/
 
 // Remove horizontal breaks between images
 hr.forEach(element => {
